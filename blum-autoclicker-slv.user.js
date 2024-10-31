9feff6c047ffbb04c76979d9d7a22641da2e59a6
SlavaSt295 <38761838+SlavaSt295@users.noreply.github.com>
2024-11-01 00:34:03 +0300

    Update and rename blum-autoclicker-slv41.user.js to blum-autoclicker-slv.user.js

--- blum-autoclicker-slv.user.js
+++ blum-autoclicker-slv.user.js
@@ -1,13 +1,13 @@
 // ==UserScript==
 // @name         Blum Autoclicker
-// @version      2.4
+// @version      999
 // @namespace    Violentmonkey Scripts
 // @author       mudachyo
 // @match        https://telegram.blum.codes/*
 // @grant        none
 // @icon         https://cdn.prod.website-files.com/65b6a1a4a0e2af577bccce96/65ba99c1616e21b24009b86c_blum-256.png
-// @downloadURL  https://github.com/mudachyo/Blum/raw/main/blum-autoclicker.user.js
-// @updateURL    https://github.com/mudachyo/Blum/raw/main/blum-autoclicker.user.js
+// @downloadURL  https://github.com/SlavaSt295/Blum/blob/main/blum-autoclicker-slv.user.js
+// @updateURL    https://github.com/SlavaSt295/Blum/blob/main/blum-autoclicker-slv.user.js
 // @homepage     https://github.com/mudachyo/Blum
 // ==/UserScript==
 

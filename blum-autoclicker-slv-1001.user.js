f7f773739f8cfaf8540c837f297592cae3b17230
SlavaSt295 <38761838+SlavaSt295@users.noreply.github.com>
2024-11-01 00:52:10 +0300

    Update and rename blum-autoclicker-slv.user.js to blum-autoclicker-slv-1001.user.js

--- blum-autoclicker-slv-1001.user.js
+++ blum-autoclicker-slv-1001.user.js
@@ -6,8 +6,8 @@
 // @match        https://telegram.blum.codes/*
 // @grant        none
 // @icon         https://cdn.prod.website-files.com/65b6a1a4a0e2af577bccce96/65ba99c1616e21b24009b86c_blum-256.png
-// @downloadURL  https://github.com/SlavaSt295/Blum/blob/main/blum-autoclicker-slv.user.js
-// @updateURL    https://github.com/SlavaSt295/Blum/blob/main/blum-autoclicker-slv.user.js
+// @downloadURL  https://github.com/SlavaSt295/Blum/blob/main/blum-autoclicker-slv-1001.user.js
+// @updateURL    https://github.com/SlavaSt295/Blum/blob/main/blum-autoclicker-slv-1001.user.js
 // @homepage     https://github.com/mudachyo/Blum
 // ==/UserScript==
 
